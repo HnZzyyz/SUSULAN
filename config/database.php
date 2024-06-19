84d968068d5131ddce744afe710323a914abd8d9
HnZzyyz <aldairsyafie@gmail.com>
2024-06-19 11:03:26 +0700

    first commit

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "UPRAK_MUHAMADFAUZI";
+
+// Create connection
+$connect = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+} else {
+    // echo "Connected successfully";
+}
+?>
